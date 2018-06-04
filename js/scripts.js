d97ef9340cb334f1392168061cb4545af4b1ae31
Anousone Kaseumsouk and Lan Dam <lan@>
2018-06-04 15:18:17 -0700

    Enter code to collect user input from the form, split into individual words, store any words greater or equal to three, reverse and join back into string and then output.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   $("#word-play").submit(function(event){
     event.preventDefault();
-
-
+    var userInput = $("input#sentence").val();
+    var splitStrings = userInput.split(" ");
+    var newArrays = splitStrings.filter(string => string.length >= 3);
+    newArrays.reverse();
+    var newString = newArrays.join();
+    $("#output").text(newString);
 
   });
-
-
-
-
 
 });
